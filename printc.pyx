542411c7a0be2bc86cff146dd2f05d2a53f9e45f
mc_fdc <45692264+tuna2134@users.noreply.github.com>
2022-04-24 00:48:17 +0900

    Update printc.pyx

--- printc.pyx
+++ printc.pyx
@@ -1,7 +1,7 @@
 import cython
 
 cdef extern from "print.h":
-    char print(char word, char color)
+    int print(char word, char color)
     
 def print(char word, char color):
     print(word, color)
