a6d227496c52e63a61f534682c6c67d0dfaf0831
mc_fdc <45692264+tuna2134@users.noreply.github.com>
2022-04-24 10:24:26 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup, Extension
+from setuptools import setup, Extension
 from Cython.Build import cythonize
 
 ext = Extension("sample", sources=["printc.pyx", "print.c"], include_dirs=['.'])
