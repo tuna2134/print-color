35571f9908e08ed1715421b0148bda9e61021deb
mc_fdc <45692264+tuna2134@users.noreply.github.com>
2022-04-24 10:12:58 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from distutils.core import setup, Extension
 from Cython.Build import cythonize
 
-ext = Extension("sample", sources=["printc.pyx", "print.c"], include_dirs=['.'])
+ext = Extension("pc", sources=["printc.pyx", "print.c"], include_dirs=['.'])
 setup(name="printcolor", ext_modules=cythonize([ext]))
