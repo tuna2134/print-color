3430112b2a32278f4450f3e15d58600e8e909ad3
mc_fdc <45692264+tuna2134@users.noreply.github.com>
2022-04-24 10:26:14 +0900

    Update print.c

--- print.c
+++ print.c
@@ -5,7 +5,7 @@ int printc(char word, char color){
     {
         printf("\x1b[40m");
     }
-    else if(str(color, "red") == 0)
+    else if(strcmp(color, "red") == 0)
     {
         printf("\x1b[41m");
     }
